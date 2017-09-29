89fef65f301bf0546406ca9c5750935f0b18f91e
Jaden Goodwin <Jadengoodwin24@Jadens-MacBook-Air.local>
2017-09-29 11:48:15 -0600

    more

--- houser-app/src/router.js
+++ houser-app/src/router.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
+import Login from './components/Login';
 import Wizard1 from './components/Wizard1';
 import Wizard2 from './components/Wizard2';
 import Wizard3 from './components/Wizard3';
@@ -8,6 +10,8 @@ import Wizard5 from './components/Wizard5';
 
 export default (
     <Switch>
+        
+        <Route exact path="/login" component={Login} />
         <Route exact path="/step1" component={Wizard1} />
         <Route exact path="/step2" component={Wizard2} />
         <Route exact path="/step3" component={Wizard3} />
