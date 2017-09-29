361af618e06e0b7683234c432c35fdd3aaa745d7
Jaden Goodwin <Jadengoodwin24@Jadens-MacBook-Air.local>
2017-09-29 11:31:20 -0600

    added components

---
+++ houser-app/src/router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Wizard1 from './components/Wizard1';
+import Wizard2 from './components/Wizard2';
+import Wizard3 from './components/Wizard3';
+import Wizard4 from './components/Wizard4';
+import Wizard5 from './components/Wizard5';
+
+export default (
+    <Switch>
+        <Route exact path="/step1" component={Wizard1} />
+        <Route exact path="/step2" component={Wizard2} />
+        <Route exact path="/step3" component={Wizard3} />
+        <Route exact path="/step4" component={Wizard4} />
+        <Route exact path="/step5" component={Wizard5} />
+        </Switch>
+)
